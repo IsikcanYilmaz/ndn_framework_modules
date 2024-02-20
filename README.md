6427838e5a74c06043356bdac6fef5549963e7ad
jon <isikcan.yilmaz@tu-dresden.de>
2024-02-20 17:34:36 +0100

    added submodules and setup script

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ndn_framework_modules
+
+- NDN Forwarding Daemon (NFD)
+- Named Data Link State Routing (NLSR)
+- NDN Essential Tools (ndn-tools)
+- NDN Traffic Generator
+- infoedit
+- Mini-NDN (which depends on Mininet, but is not in this repo)
---
+++ setup.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+echo "[*] Getting submodules"
+git submodule update --recursive --init
+
+echo "[?] Should the built software be installed? (as opposed to just built) (y/n)"
+read doInstall
+
+###############################
+# ndn-cxx # https://docs.named-data.net/ndn-cxx/current/INSTALL.html#
+echo "[*] Installing ndn-cxx"
+sudo apt install build-essential pkg-config python3-minimal libboost-all-dev libssl-dev libsqlite3-dev
+cd ndn-cxx
+./waf configure --with-examples --with-tests
+./waf
+if [ "$doInstall" == "y" ]; then
+	sudo ./waf install
+	sudo ldconfig
+fi
+cd ..
+
+###############################
+# NFD # https://docs.named-data.net/NFD/current/INSTALL.html
+echo "[*] Installing NFD"
+sudo apt install libpcap-dev libsystemd-dev
+cd NFD
+./waf configure
+./waf
+if [ "$doInstall" == "y" ]; then
+	sudo ./waf install
+	sudo cp /usr/local/etc/ndn/nfd.conf.sample /usr/local/etc/ndn/nfd.conf
+fi
+cd ..
+
+###############################
+# PSync # https://github.com/named-data/PSync#build
+echo "[*] Installing PSync"
+cd PSync
+./waf configure --with-examples
+./waf 
+if [ "$doInstall" == "y" ]; then
+	sudo ./waf install
+fi
+cd ..
+
+###############################
+# ndn-svs # https://github.com/named-data/ndn-svs?tab=readme-ov-file#installation
+echo "[*] Installing ndn-svs"
+cd ndn-svs
+./waf configure --with-examples
+./waf
+if [ "$doInstall" == "y" ]; then
+	sudo ./waf install
+fi
+cd ..
+
+###############################
+# NLSR # https://docs.named-data.net/NLSR/current/INSTALL.html
+echo "[*] Installing NLSR"
+cd NLSR
+./waf configure --with-psync --with-svs --with-tests
+./waf
+if [ "$doInstall" == "y" ]; then
+	sudo ./waf install
+fi
+cd ..
+
+###############################
+# ndn-tools # https://github.com/named-data/ndn-tools
+echo "[*] Installing ndn-tools"
+cd ndn-tools
+./waf configure --with-tests
+./waf
+if [ "$doInstall" == "y" ]; then
+	sudo ./waf install
+fi
+cd ..
+
+###############################
+# ndn-traffic-generator # https://github.com/named-data/ndn-traffic-generator
+echo "[*] Installing ndn-traffic-generator"
+cd ndn-traffic-generator
+./waf configure
+./waf
+if [ "$doInstall" == "y" ]; then
+	sudo ./waf install
+fi
+cd ..
+
+###############################
+# infoedit # https://github.com/NDN-Routing/infoedit
+echo "[*] Installing infoedit"
+cd infoedit
+make
+if [ "$doInstall" == "y" ]; then
+	sudo make install
+fi
+cd ..
+
+###############################
+
+echo "[?] Install Mini-NDN? (y/n)"
+read doInstallMinindn
+if [ "$doInstallMinindn" == "y" ]; then
+	cd mini-ndn
+	./install.sh
+fi
+
+###############################
+echo "[*] Done"
