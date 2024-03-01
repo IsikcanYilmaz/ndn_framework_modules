653e993e4bbaf5d2a5d5fc4e66f6dc3b8b41747e
jon <isikcan.yilmaz@tu-dresden.de>
2024-03-01 16:42:49 +0100

    updates to utils. netcat based file transfers work

--- mystuff/ndn_cache_test.py
+++ mystuff/ndn_cache_test.py
@@ -22,13 +22,38 @@ from minindn.helpers.ip_routing_helper import IPRoutingHelper
 from mytopologies.basic import *
 from mytopologies.ndn_cache_topo import *
 
-PREFIX = "/example"
-   
+import util
+
+PREFIX : str = "/example"
+NUM_TRIALS : int = 5
+
 def runMininet(topo):
     net = Mininet(topo = topo, waitConnected = True)
 
     net.start()
-    CLI(net)
+
+    hosts : array = net.hosts
+    print("hosts:", hosts)
+
+    producer : Node = net.getNodeByName('h0_0')
+    consumer : Node = net.getNodeByName('h3_3')
+    filename : str = "payload.bin"
+    recvFilename : str = "received_payload.bin"
+    sizeBytes : int = 100 * 1024 
+    
+    util.nodeTest(producer)
+    util.nodeTest(consumer)
+    util.createPayload(node=producer, filename=filename, sizeBytes=sizeBytes)
+    util.listenForFile(node=consumer, filename=recvFilename)
+
+    print(f'{producer.name} transferring {sizeBytes} Bytes to {consumer.name} at {consumer.IP()}') 
+    for i in range(0, NUM_TRIALS):
+        t0 = time.time()
+        util.sendFile(node=producer, destNode=consumer, filename=filename)
+        t1 = time.time()
+        print(f'Trial {i}: File transfer in {t1-t0} ms')
+
+    # CLI(net)
     net.stop()
 
 def runMinindn(topo):
@@ -45,6 +70,35 @@ def runMinindn(topo):
     time.sleep(sleepSecs)
     IPRoutingHelper.calcAllRoutes(ndn.net)
 
+    hosts = ndn.net.hosts
+    print("hosts:", hosts)
+
+    producer : Node = ndn.net.getNodeByName('h0_0')
+    consumer : Node = ndn.net.getNodeByName('h3_3')
+    filename = "payload.bin"
+    recvFilename = "received_payload.bin"
+    sizeBytes = 100 * 1024 
+    
+    util.nodeTest(producer)
+    util.nodeTest(consumer)
+    util.createPayload(node=producer, filename=filename, sizeBytes=sizeBytes)
+    util.listenForFile(node=consumer, filename=recvFilename)
+
+    print(f'{producer.name} transferring {sizeBytes} Bytes to {consumer.name} at {consumer.IP()}') 
+    for i in range(0, NUM_TRIALS):
+        t0 = time.time()
+        util.sendFile(node=producer, destNode=consumer, filename=filename)
+        t1 = time.time()
+        print(f'Trial {i}: File transfer in {t1-t0} ms')
+    
+    print(f'{producer.name} starting NDN filetransfer')
+    producer.cmd(f'nlsrc advertise {PREFIX}')
+    producer.cmd(f'cat {filename} | ndnpoke {PREFIX} &> producer.log &')
+    t0 = time.time()
+    consumer.cmd(f'ndnpeek -p {PREFIX} &> {recvFilename}')
+    t1 = time.time()
+    print(f'NDN File transfer in {t1-t0} ms')
+
     MiniNDNCLI(ndn.net)
     ndn.stop()
     
@@ -53,7 +107,8 @@ if __name__ == "__main__":
     # topo = createTopologySimpleTree(ndn=True)
     # topo = createTopologyComplex(ndn=True)
     # topo = createTopologyPointToPoint()
-    topo = createNdnCacheTopo(ndn=False)
-    runMininet(topo)
-    # runMinindn(topo)
+    # topo = createNdnCacheTopo(ndn=False)
+    # runMininet(topo)
+    topo = createNdnCacheTopo(ndn=True)
+    runMinindn(topo)
 
--- mystuff/util.py
+++ mystuff/util.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3 
 
-import sys, os
+import sys, os, shlex
+import subprocess
+import time
+
 from mininet.node import Node
 
 NC_PORT = 12345
 
-def createPayload(sizeBytes: int = 4096, 
+def nodeTest(node: Node):
+    node.cmd(f'echo Node {node.name} at {node.IP()}')
+
+def createPayload(node: Node = None, 
+                  sizeBytes: int = 4096, 
                   filename: str = "payload.bin"
                   ) -> None:
-    os.system(f'dd if=/dev/urandom of={filename} count={sizeBytes} bs=1')
+    cmd = f'dd if=/dev/urandom of={filename} count={sizeBytes} bs=1'
+    if node is not None:
+        node.cmd(cmd)
+    else:
+        os.system(cmd)
 
 def listenForFile(node: Node = None,
                   filename: str = "received_payload.bin",
                   port: int = 12345
                   ):
-    # nc -l -p 12345 > received_payload.img
+    # nc -l -p 12345 > received_payload.bin
     # -l : listen, -p : port
-    cmd = f'nc -l -p -q {port} > {filename}'
+    cmd = f'nc -l -p {port} > {filename} &'
+    # print(">", cmd)
 
     # If this is being called thru mininet
     if node is not None:
-        node.sendCmd(cmd)
+        node.cmd(cmd)
     else:
         os.system(cmd)
 
@@ -34,11 +46,17 @@ def sendFile(node: Node = None,
     dest = destNode.IP() if destNode is not None else destIp
     if dest is None:
         print("Need either a destionation mininet node object or a destination IP")
-    cmd = f'nc {dest} {port} < {filename}'
+
+    # nc -w0 10.0.0.2 12345 < payload.bin
+    # -w0 exit after EOF
+    cmd = f'nc -w0 {dest} {port} < {filename}'
 
     # If this is being called thru mininet
     if node is not None:
-        node.sendCmd(cmd)
+        node.cmd(cmd)
     else:
         os.system(cmd)
+        # print(output.stdout)
 
+def test():
+    pass
